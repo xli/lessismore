0d0ab6b503e28b33427b072e63409e38f44ab832
Xiao Li <swing1979@gmail.com>
2013-05-24 23:18:19 -0700

    script to pull repository into data dir

--- app/models/commit_log.rb
+++ app/models/commit_log.rb
@@ -1,4 +1,8 @@
 class CommitLog < ActiveRecord::Base
+  def identifier
+    "#{id}_#{name.gsub(/\W/, '_')}"
+  end
+
   def safe_repository_url
     repository_url.to_s.gsub(/:[^@:]*@/, ':*****@')
   end
