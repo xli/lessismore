479a9b84f39f2e95e810990a2f00abe07ac5c75f
Xiao Li <swing1979@gmail.com>
2013-05-29 23:25:36 -0700

    show a email text based default avatar instead of a random pic for the commit author

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper
   def gravatar(author, size)
-    email = author.email || extract_email(author.name.to_s) || author.name
-    gravatar_image_tag(email, class: "img-rounded", alt: email, gravatar: { size: size })
+    email = author.email.strip.downcase
+    code = Rails.cache.fetch("avatar:#{email}", :expires_in => 1.hour) do
+      HTTParty.get(GravatarImageTag::gravatar_url(email, default: 404)).code
+    end
+    if code == 404
+      content_tag('div', email, class: 'default-avatar')
+    else
+      gravatar_image_tag(email, class: "img-rounded", alt: author.email, gravatar: { size: size })
+    end
   end
 
   def extract_email(txt)
